530e2285d97137dfb29053299182817d6066cf5b
ashishsbtambe <ashishsbtambe@gmail.com>
2018-11-28 18:31:09 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
 <?php
+require 'vendor/autoload.php';
 
-header('Location: ./demos/index.php');
+$app = new \atk4\ui\App();   // That's your UI application
+$app->initLayout('Centered');
+
+$form = new \atk4\ui\Form(); // Yeah, that's a form!
+$app->add($form);
+
+$form->addField('email');    // adds field
+$form->onSubmit(function ($form) {
+    // implement subscribe here
+
+    return $form->success('Subscribed '.$form->model['email'].' to newsletter.');
+});
+
+// Decorate anything
+$form->buttonSave->set('Subscribe');
+$form->buttonSave->icon = 'mail';
+
+// everything renders automatically
